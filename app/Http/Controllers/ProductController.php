f71289f77c8ca235fb01b67f80a4f2c67f6843db
Tony Jemba <jjembatony@gmail.com>
2022-03-17 11:28:27 +0300

    adding product controller functionality

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -13,7 +14,7 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        return Product::all();
     }
 
     /**
@@ -24,7 +25,17 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'product_name' => 'required',
+            'Pharmacy_name' => 'required',
+            'location' => 'required',
+            'price' => 'required',
+            'disease' => 'required',
+            'descprition' => 'required',
+            'contact' => 'required',
+        ]);
+
+        return Product::create($request->all());
     }
 
     /**
@@ -35,7 +46,7 @@ class ProductController extends Controller
      */
     public function show($id)
     {
-        //
+        return Product::find($id);
     }
 
     /**
@@ -47,7 +58,11 @@ class ProductController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $product = Product::find($id);
+
+        $product->update($request->all());
+
+        return $product;
     }
 
     /**
@@ -58,6 +73,17 @@ class ProductController extends Controller
      */
     public function destroy($id)
     {
-        //
+        return Product::destroy($id);
+    }
+
+    /*
+    search for a specific product
+    */
+
+    public function search($name){
+
+        $product = Product::where('product_name','like','%'.$name.'%');
+
+        return $product;
     }
 }
