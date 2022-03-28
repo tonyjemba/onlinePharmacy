bb53a33185ec0500aa19f4442ab824e7d892d1be
Tony Jemba <jjembatony@gmail.com>
2022-03-28 14:48:10 +0300

    working routes

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -74,9 +74,9 @@ router.beforeEach((to, from, next) => {
         next({ name: "Login" });
     } 
     //user will be redirected to register if he is not registered
-    if (to.meta.auth && !registered ) {
-        next({ name: "Register" });
-    }
+    // if (to.meta.auth && !registered ) {
+    //     next({ name: "Register" });
+    // }
     //if the user is loggedin will move to the dashboard route defined as the next
     else if (loggedin && (to.name === "Login" || to.name === "Register")) {
         next({ name: "Dashboard" });
