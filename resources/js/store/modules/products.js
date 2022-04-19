74bd39c4a1da49a92d776b15d9bb0970acfd8251
Tony Jemba <jjembatony@gmail.com>
2022-04-19 20:27:26 +0300

    adding btn state

---
+++ resources/js/store/modules/products.js
@@ -0,0 +1,56 @@
+import axios from "axios";
+import store from "../index";
+import router from "../../router/index";
+// root state object.
+// each Vuex instance is just a single state tree.
+const state = {
+  
+  }
+  
+  // mutations are operations that actually mutate the state.
+  // each mutation handler gets the entire state tree as the
+  // first argument, followed by additional payload arguments.
+  // mutations must be synchronous and can be recorded by plugins
+  // for debugging purposes.
+  const mutations = {
+
+  }
+  
+  // actions are functions that cause side effects and can involve
+  // asynchronous operations.
+  const actions = {
+    storeProduct({ commit, state }, payload) {
+ 
+        //making api request
+        axios
+            .post("https://online-pharmacy-project.herokuapp.com/api/add-product", payload)
+            .then((response) => {
+                commit("REG_DATA", response);
+                commit("MSG", response.data.message);
+                commit("REGISTERED");
+                router.push("/login");
+            })
+            .catch((error) => {
+                commit("Error", error.response.data.message);
+                commit("ROUTE_LOADING", false);
+            });
+    },
+  }
+  
+  // getters are functions.
+  const getters = {
+ 
+ 
+  }
+  
+  // A Vuex instance is created by combining the state, mutations, actions,
+  // and getters.
+  const products =  {
+      namespaced: true,
+      state,
+      getters,
+      actions,
+      mutations
+    };
+  
+    export default products;
--- routes/api.php
+++ routes/api.php
@@ -25,7 +25,7 @@ Route::post('register',[AuthController::class, 'register']);
 Route::post('login',[AuthController::class, 'login']);
 Route::post('logout',[AuthController::class, 'logout']);
 
-Route::resource('products', ProductController::class);
+Route::resource('add-product',[ ProductController::class, "store"]);
 
 //protected routes group (post products)
 Route::group(['middleware' => ['auth:sanctum']], function(){
