ee6bfe5b71e4aaa18bb8eb348c7237fbc351e074
Tony Jemba <jjembatony@gmail.com>
2022-04-19 20:36:36 +0300

    adding btn state3

--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -1,8 +1,7 @@
 import { createStore, createLogger } from 'vuex';
 import register from './modules/register';
 import login from './modules/login';
-import logout from "./modules/logout";
-import products from './modules/products';
+import logout from "./modules/logout";z
 // for state persistence
 import VuexPersist from 'vuex-persist';
 
@@ -19,8 +18,7 @@ const store = createStore({
   modules: {
     register,
     login,
-    logout,
-    products
+    logout
   },
   strict: debug,
   plugins: debug ? [createLogger(),vuexLocalStorage.plugin] : [createLogger(),vuexLocalStorage.plugin]
