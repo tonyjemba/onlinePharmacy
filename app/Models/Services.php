7062f3a861f78ba2c35b0d99f2ea95bd0ec7493a
Tony Jemba <jjembatony@gmail.com>
2022-04-23 01:12:09 +0300

    adding the services model

---
+++ app/Models/Services.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Services extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'service_name',
+        'Pharmacy_name',
+        'location',
+        'price',
+        'disease',
+        'descprition',
+        'contact',
+        'image_url'
+    ];
+}
