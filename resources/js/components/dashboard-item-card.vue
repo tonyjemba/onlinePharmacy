833b0284a490f5d046516aa9aba477fcbb24a838
Tony Jemba <jjembatony@gmail.com>
2022-04-26 23:50:36 +0300

    adding comments

--- resources/js/components/dashboard-item-card.vue
+++ resources/js/components/dashboard-item-card.vue
@@ -1,6 +1,7 @@
 
 
 <template>
+<!-- component for dashboard item -->
  <div class="bg-white rounded-lg border shadow-md max-w-xs md:max-w-none overflow-hidden">
             <img class="h-56 lg:h-60 w-full object-cover" src="https://images.unsplash.com/photo-1523289217630-0dd16184af8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d29tZW4lMjBlbXBvd2VybWVudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
             <div class="p-3">
--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -31,7 +31,7 @@
     <div
         class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10"
     >
-        
+       <!-- display data from databese in dashbaord item  -->
         <!-- Card 1 -->
         <DashboardItemCard />
 
