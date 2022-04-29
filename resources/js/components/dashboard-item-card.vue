fdc41808f0be7b6281f14df62fe88c19f15b7457
Tony Jemba <jjembatony@gmail.com>
2022-04-29 22:14:08 +0300

    displaying products

--- resources/js/components/dashboard-item-card.vue
+++ resources/js/components/dashboard-item-card.vue
@@ -3,16 +3,16 @@
 <template>
 <!-- component for dashboard item -->
  <div class="bg-white rounded-lg border shadow-md max-w-xs md:max-w-none overflow-hidden">
-            <img class="h-56 lg:h-60 w-full object-cover" src="https://images.unsplash.com/photo-1523289217630-0dd16184af8e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OHx8d29tZW4lMjBlbXBvd2VybWVudHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+            <img class="h-56 lg:h-60 w-full object-cover" :src=image_url  alt="" />
             <div class="p-3">
-                <span class="text-sm text-primary">November 19, 2022</span>
+                <span class="text-sm text-primary">Last Update:{{ updated_at }}</span>
                 <h3 class="font-semibold text-xl leading-6 text-gray-700 my-2">
-                International Women's Day 2022: Date, history, significance, theme this year
+                {{product_name }}
                 </h3>
                 <p class="paragraph-normal text-gray-600">
-                Happy Women's Day 2022: Read on to know all about the history and significance...
+               {{ descprition }}
                 </p>
-                <a class="mt-3 block" href="#">Read More >></a>
+                <div class="mt-3 block" >UGX: {{price}}</div>
                 <div className="flex items-center justify-end mt-4 top-auto">
             <button className="bg-white text-red-500 px-4 py-2 rounded mr-auto hover:underline">Delete</button>
             <button className=" bg-gray-200 text-blue-600 px-2 py-2 rounded-md mr-2">Edit</button>
@@ -26,7 +26,7 @@
 
 
 export default {
-
+props: ['product_name','updated_at','descprition','price','image_url'],
  
 }
 </script>
--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -5,27 +5,19 @@
             <div
                 class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10"
             >
-                <!-- display data from databese in dashbaord item  -->
-                <!-- Card 1 -->
-                <DashboardItemCard />
-
-                <!-- Card 2 -->
-                <DashboardItemCard />
-                <!-- Card 3 -->
-                <DashboardItemCard />
-
-                <!-- Card 4 -->
-                <DashboardItemCard />
-
-                <!-- Card 5 -->
-                <DashboardItemCard />
-
-                <!-- Card 6 -->
-                <DashboardItemCard />
+                <div v-for="product in products" :key="product.id">
+                    <DashboardItemCard
+                        :product_name="product.product_name"
+                        :descprition="product.descprition"
+                        :image_url="product.image_url"
+                        :price="product.price"
+                        :updated_at="product.updated_at"
+                    />
+                </div>
             </div>
         </div>
         <div>
-            <h1 class="text-1xltext-2xl md:text-black">My services</h1>
+            <h1 class="text-1xl md:text-2xl text-black">My services</h1>
             <div
                 class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10"
             >
@@ -54,17 +46,16 @@
 <script>
 import DashboardItemCard from "../components/dashboard-item-card.vue";
 import { useStore } from "vuex";
-import { onMounted ,computed} from "vue";
+import { onMounted, computed } from "vue";
 
 export default {
     setup() {
         const store = useStore();
         onMounted(() => {
-         
-                store.dispatch("products/fetchProcucts");
+            store.dispatch("products/fetchProcucts");
         });
         return {
-          products: computed(() => store.state.products.products)
+            products: computed(() => store.state.products.products),
         };
     },
     components: {
