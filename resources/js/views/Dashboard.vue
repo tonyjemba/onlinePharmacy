7bbd3fdd2b2fb633935c925a16584bc60484d1b8
Tony Jemba <jjembatony@gmail.com>
2022-04-29 20:05:26 +0300

    updating structure

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -1,10 +1,34 @@
 <template>
-<h1 class="text-1xl md:text-2xl text-white mb-5">My products</h1>
+    <div>
+        <div>
+            <h1 class="text-1xl md:text-2xl text-white ">My products</h1>
+            <div
+                class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10"
+            >
+                <!-- display data from databese in dashbaord item  -->
+                <!-- Card 1 -->
+                <DashboardItemCard />
 
-    <div
+                <!-- Card 2 -->
+                <DashboardItemCard />
+                <!-- Card 3 -->
+                <DashboardItemCard />
+
+                <!-- Card 4 -->
+                <DashboardItemCard />
+
+                <!-- Card 5 -->
+                <DashboardItemCard />
+
+                <!-- Card 6 -->
+                <DashboardItemCard />
+            </div>
+        </div>
+        <div>
+            <h1 class="text-1xltext-2xl  md:text-white ">My services</h1>
+            <div
         class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10"
     >
-       <!-- display data from databese in dashbaord item  -->
         <!-- Card 1 -->
         <DashboardItemCard />
 
@@ -12,7 +36,7 @@
         <DashboardItemCard />
         <!-- Card 3 -->
         <DashboardItemCard />
-        
+
         <!-- Card 4 -->
         <DashboardItemCard />
 
@@ -22,28 +46,7 @@
         <!-- Card 6 -->
         <DashboardItemCard />
     </div>
-    <h1 class="text-1xl md:text-2xl text-white mb-5">My services</h1>
-    
-        <div
-        class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10"
-    >
-        
-        <!-- Card 1 -->
-        <DashboardItemCard />
-
-        <!-- Card 2 -->
-        <DashboardItemCard />
-        <!-- Card 3 -->
-        <DashboardItemCard />
-        
-        <!-- Card 4 -->
-        <DashboardItemCard />
-
-        <!-- Card 5 -->
-        <DashboardItemCard />
-
-        <!-- Card 6 -->
-        <DashboardItemCard />
+        </div>
     </div>
 </template>
 
