31a2ac5c715aa6a40458776fcbc144a06d82f0b3
Tony Jemba <jjembatony@gmail.com>
2022-04-30 08:44:45 +0300

    adding searching component

--- resources/js/views/Dashboard.vue
+++ resources/js/views/Dashboard.vue
@@ -3,6 +3,35 @@
         <div>
             <h1 class="text-1xl md:text-2xl text-black">My products</h1>
             <div
+                class="flex min-h-screen w-full items-center justify-center bg-gray-900"
+            >
+                <!-- component -->
+                <div
+                    class="flex rounded bg-white w-[30rem]"
+                    x-data="{ search: '' }"
+                >
+                    <input
+                        type="search"
+                        class="w-full border-none bg-transparent px-4 py-1 text-gray-900 outline-none focus:outline-none"
+                        placeholder="Find product"
+                        x-model="search"
+                    />
+
+                    <button
+                        type="button"
+                        class="m-2 rounded px-4 px-4 py-2 font-semibold text-gray-100"
+                        :class="
+                            search.length > 0
+                                ? 'bg-purple-500'
+                                : 'bg-gray-500 cursor-not-allowed'
+                        "
+                        :disabled="search.length == 0"
+                    >
+                        search
+                    </button>
+                </div>
+            </div>
+            <div
                 class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10"
             >
                 <div v-for="product in products" :key="product.id">
@@ -18,6 +47,35 @@
         </div>
         <div>
             <h1 class="text-1xl md:text-2xl text-black">My services</h1>
+               <div
+                class="flex min-h-screen w-full items-center justify-center bg-gray-900"
+            >
+                <!-- component -->
+                <div
+                    class="flex rounded bg-white w-[30rem]"
+                    x-data="{ search: '' }"
+                >
+                    <input
+                        type="search"
+                        class="w-full border-none bg-transparent px-4 py-1 text-gray-900 outline-none focus:outline-none"
+                        placeholder="Find service"
+                        x-model="search"
+                    />
+
+                    <button
+                        type="button"
+                        class="m-2 rounded px-4 px-4 py-2 font-semibold text-gray-100"
+                        :class="
+                            search.length > 0
+                                ? 'bg-purple-500'
+                                : 'bg-gray-500 cursor-not-allowed'
+                        "
+                        :disabled="search.length == 0"
+                    >
+                        search
+                    </button>
+                </div>
+            </div>
             <div
                 class="grid justify-center md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-7 my-10"
             >
@@ -30,7 +88,6 @@
                         :updated_at="service.updated_at"
                     />
                 </div>
-              
             </div>
         </div>
     </div>
