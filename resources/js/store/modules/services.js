4c478eb4c0bae43e2fa8f06942c36e46e65e03b4
Tony Jemba <jjembatony@gmail.com>
2022-05-04 07:35:36 +0300

    making a delete route

--- resources/js/store/modules/services.js
+++ resources/js/store/modules/services.js
@@ -56,7 +56,7 @@ const state = {
   deleteService({ commit, state }, payload) {
     //making api request
     axios
-        .post("https://online-pharmacy-project.herokuapp.com/api/services", payload)
+        .delete("https://online-pharmacy-project.herokuapp.com/api/services", payload)
         .then(() => {
           //refreshes currrent page
           router.go();
