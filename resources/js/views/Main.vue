971065bf5d11f9ed7569530e7d274900ee4c0a06
Tony Jemba <jjembatony@gmail.com>
2022-05-17 00:31:23 +0300

    main2

--- resources/js/views/Main.vue
+++ resources/js/views/Main.vue
@@ -8,94 +8,101 @@
                             <Icon :height="50" :width="50" :autoPlay="false" />
                         </div>
                         <div class="hidden md:block">
-                            <div class="ml-10 flex items-baseline space-x-4">
-                                <div
-                                    v-for="item in navigation"
-                                    :key="item.name"
-                                    :class="[
-                                        this.$route.path == item.route
-                                            ? 'bg-indigo-900 text-white'
-                                            : 'text-gray-800 hover:bg-indigo-200 hover:text-white',
-                                        'px-3 py-2 rounded-md text-sm font-medium',
-                                    ]"
-                                    :aria-current="
-                                        item.current ? 'page' : undefined
-                                    "
-                                >
-                                    <router-link
-                                        :to="item.route"
-                                        @click="tab = item.name"
-                                        >{{ item.name }}</router-link
-                                    >
-                                </div>
-                            </div>
+                            <h1 class="text-3xl font-bold text-gray-900">
+                                Online Pharmacy
+                            </h1>
                         </div>
                     </div>
 
                     <div class="hidden md:block">
-                        <div class="ml-4 flex items-center md:ml-6">
-                            <button
-                                type="button"
-                                class="bg-indigo-500 p-1 rounded-full text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                            >
-                                <span class="sr-only">Go Home</span>
-                                <router-link to="/">
-                                    <HomeIcon
-                                        class="h-6 w-6"
-                                        aria-hidden="true"
-                                    />
-                                </router-link>
-                            </button>
-
-                            <!-- Profile dropdown -->
-                            <Menu as="div" class="ml-6 relative">
-                                <div>
-                                    <MenuButton
-                                        class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                                    >
-                                        <span class="sr-only"
-                                            >Open user menu</span
-                                        >
-                                        <img
-                                            class="h-8 w-8 rounded-full"
-                                            :src="imageUrl"
-                                            alt=""
-                                        />
-                                    </MenuButton>
-                                </div>
-                                <transition
-                                    enter-active-class="transition ease-out duration-100"
-                                    enter-from-class="transform opacity-0 scale-95"
-                                    enter-to-class="transform opacity-100 scale-100"
-                                    leave-active-class="transition ease-in duration-75"
-                                    leave-from-class="transform opacity-100 scale-100"
-                                    leave-to-class="transform opacity-0 scale-95"
+                        <div v-if="user !== []">
+                            <div class="ml-4 flex items-center md:ml-6">
+                                <button
+                                    type="button"
+                                    class="bg-indigo-500 p-1 rounded-full text-gray-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                                 >
-                                    <MenuItems
-                                        class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
-                                    >
-                                        <MenuItem
-                                            v-for="item in userNavigation"
-                                            :key="item.name"
-                                            @click="tab = item.name"
-                                            v-slot="{ active }"
+                                    <span class="sr-only">Go Dashboard</span>
+                                    <router-link to="/dashboard">
+                                        <HomeIcon
+                                            class="h-6 w-6"
+                                            aria-hidden="true"
+                                        />
+                                    </router-link>
+                                </button>
+
+                                <!-- Profile dropdown -->
+                                <Menu as="div" class="ml-6 relative">
+                                    <div>
+                                        <MenuButton
+                                            class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                                         >
-                                            <div
-                                                :href="item.href"
-                                                :class="[
-                                                    active ? 'bg-gray-100' : '',
-                                                    'block px-4 py-2 text-sm text-gray-800',
-                                                ]"
-                                                @click="logout(item.name)"
+                                            <span class="sr-only"
+                                                >Open user menu</span
                                             >
-                                                <router-link :to="item.route">{{
-                                                    item.name
-                                                }}</router-link>
-                                            </div>
-                                        </MenuItem>
-                                    </MenuItems>
-                                </transition>
-                            </Menu>
+                                            <img
+                                                class="h-8 w-8 rounded-full"
+                                                :src="imageUrl"
+                                                alt=""
+                                            />
+                                        </MenuButton>
+                                    </div>
+                                    <transition
+                                        enter-active-class="transition ease-out duration-100"
+                                        enter-from-class="transform opacity-0 scale-95"
+                                        enter-to-class="transform opacity-100 scale-100"
+                                        leave-active-class="transition ease-in duration-75"
+                                        leave-from-class="transform opacity-100 scale-100"
+                                        leave-to-class="transform opacity-0 scale-95"
+                                    >
+                                        <MenuItems
+                                            class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+                                        >
+                                            <MenuItem
+                                                v-for="item in userNavigation"
+                                                :key="item.name"
+                                                @click="tab = item.name"
+                                                v-slot="{ active }"
+                                            >
+                                                <div
+                                                    :href="item.href"
+                                                    :class="[
+                                                        active
+                                                            ? 'bg-gray-100'
+                                                            : '',
+                                                        'block px-4 py-2 text-sm text-gray-800',
+                                                    ]"
+                                                    @click="logout(item.name)"
+                                                >
+                                                    <router-link
+                                                        :to="item.route"
+                                                        >{{
+                                                            item.name
+                                                        }}</router-link
+                                                    >
+                                                </div>
+                                            </MenuItem>
+                                        </MenuItems>
+                                    </transition>
+                                </Menu>
+                            </div>
+                        </div>
+                        <div v-else>
+                            <div>
+                                <router-link to="/login">
+                                    <DisclosureButton
+                                        class="block px-3 py-2 rounded-md text-base font-medium text-indigo-400 hover:text-white hover:bg-indigo-700"
+                                        >Login</DisclosureButton
+                                    >
+                                </router-link>
+                            </div>
+                            <div>
+                                <router-link to="/register">
+                                    <DisclosureButton
+                                        class="block px-3 py-2 rounded-md text-base font-medium text-indigo-400 hover:text-white hover:bg-indigo-700"
+                                        >Create Account</DisclosureButton
+                                    >
+                                </router-link>
+                            </div>
                         </div>
                     </div>
                     <div class="-mr-2 flex md:hidden">
@@ -120,79 +127,82 @@
             </div>
 
             <DisclosurePanel class="md:hidden">
-                <div class="px-2 pt-2 pb-3 space-y-1 sm:px-3">
-                    <DisclosureButton
-                        v-for="item in navigation"
-                        :key="item.name"
-                        as="a"
-                        :href="item.href"
-                        :class="[
-                            item.current
-                                ? 'bg-gray-900 text-white'
-                                : 'text-gray-800 hover:bg-gray-700 hover:text-white',
-                            'block px-3 py-2 rounded-md text-base font-medium',
-                        ]"
-                        :aria-current="item.current ? 'page' : undefined"
-                        ><router-link
-                            :to="item.route"
-                            @click="tab = item.name"
-                            >{{ item.name }}</router-link
-                        ></DisclosureButton
-                    >
-                </div>
-
-                <div class="pt-4 pb-3 border-t border-gray-700">
-                    <div class="flex items-center px-5">
-                        <div class="flex-shrink-0">
-                            <img
-                                class="h-10 w-10 rounded-full"
-                                :src="imageUrl"
-                                alt=""
-                            />
-                        </div>
-                        <div class="ml-3">
-                            <div
-                                class="text-base font-medium leading-none text-gray-400"
-                            >
-                                Username: {{ user.user.name }}
+                <h1 class="text-3xl font-bold text-gray-900">
+                    Online Pharmacy
+                </h1>
+                <div v-if="user !== []">
+                    <div class="pt-4 pb-3 border-t border-gray-700">
+                        <div class="flex items-center px-5">
+                            <div class="flex-shrink-0">
+                                <img
+                                    class="h-10 w-10 rounded-full"
+                                    :src="imageUrl"
+                                    alt=""
+                                />
                             </div>
+                            <div class="ml-3">
+                                <div
+                                    class="text-base font-medium leading-none text-gray-400"
+                                >
+                                    Username: {{ user.user.name }}
+                                </div>
+                                <div
+                                    class="text-sm pt-2 font-medium leading-none text-gray-400"
+                                >
+                                    Email: {{ user.user.email }}
+                                </div>
+                            </div>
+                            <button
+                                type="button"
+                                class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                            >
+                                <router-link to="/dashboard">
+                                    <HomeIcon
+                                        class="h-6 w-6"
+                                        aria-hidden="true"
+                                    />
+                                </router-link>
+                            </button>
+                        </div>
+                        <div class="ml-3 mt-2">
                             <div
                                 class="text-sm pt-2 font-medium leading-none text-gray-400"
                             >
-                                Email: {{ user.user.email }}
+                                Created: {{ user.user.updated_at }}
                             </div>
-                        </div>
-                        <button
-                            type="button"
-                            class="ml-auto bg-gray-800 flex-shrink-0 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-                        >
-                            <router-link to="/">
-                                <HomeIcon class="h-6 w-6" aria-hidden="true" />
-                            </router-link>
-                        </button>
-                    </div>
-                    <div class="ml-3 mt-2">
-                        <div
-                            class="text-sm pt-2 font-medium leading-none text-gray-400"
-                        >
-                            Created: {{ user.user.updated_at }}
-                        </div>
 
-                        <div
-                            class="text-sm pt-2 font-medium leading-none text-gray-400"
-                        >
-                            Token: {{ user.token }}
-                        </div>
-
-                        <div class="pt-4">
-                            <DisclosureButton
-                                @click="logout('Sign out')"
-                                class="block px-3 py-2 rounded-md text-base font-medium text-indigo-400 hover:text-white hover:bg-indigo-700"
-                                >Sign out</DisclosureButton
+                            <div
+                                class="text-sm pt-2 font-medium leading-none text-gray-400"
                             >
+                                Token: {{ user.token }}
+                            </div>
+
+                            <div class="pt-4">
+                                <DisclosureButton
+                                    @click="logout('Sign out')"
+                                    class="block px-3 py-2 rounded-md text-base font-medium text-indigo-400 hover:text-white hover:bg-indigo-700"
+                                    >Sign out</DisclosureButton
+                                >
+                            </div>
                         </div>
                     </div>
                 </div>
+                 <div v-else>
+                            <div>
+                                <router-link to="/login">
+                                    <DisclosureButton
+                                        class="block px-3 py-2 rounded-md text-base font-medium text-indigo-400 hover:text-white hover:bg-indigo-700"
+                                        >Login</DisclosureButton>
+                                </router-link>
+                            </div>
+                            <div>
+                                <router-link to="/register">
+                                    <DisclosureButton
+                                        class="block px-3 py-2 rounded-md text-base font-medium text-indigo-400 hover:text-white hover:bg-indigo-700"
+                                        >Create Account</DisclosureButton>
+                                </router-link>
+                            </div>
+                        </div>
             </DisclosurePanel>
         </Disclosure>
 
