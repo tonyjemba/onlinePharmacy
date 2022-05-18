f6c44f0d5524a9b73febbd51a03f4e67e4a09f96
Tony Jemba <jjembatony@gmail.com>
2022-05-18 17:02:01 +0300

    product page

--- resources/js/layout/DashboardLayout.vue
+++ resources/js/layout/DashboardLayout.vue
@@ -196,7 +196,7 @@
             </DisclosurePanel>
         </Disclosure>
 
-        <header class="bg-white shadow">
+        <header class="bg-white shadow ">
             <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                 <h1 class="text-3xl font-bold text-gray-900">{{ tab }}</h1>
             </div>
--- resources/js/views/ViewItem.vue
+++ resources/js/views/ViewItem.vue
@@ -1,21 +1,60 @@
 <template>
-  <div>{{routeId}}</div>
-  <router-link to="/">
-      <div>Back Home</div>
-  </router-link>
+    <div
+        class="container w-full flex justify-center items-center text-sm md:text-base"
+    >
+        <div class="w-11/12 sm:w-4/5 my-10 space-y-4">
+            <div>
+                <img
+                    src="https://firebasestorage.googleapis.com/v0/b/online-pharmacy-d7579.appspot.com/o/products%2Fpanadol_paracetamol_pain_relief_tablets_500mg_adva-front.jpg?alt=media&token=5f86cf93-b6ef-47ee-988f-0aab2438c883"
+                    class="object-cover shadow-lg h-64 sm:h-80 w-full"
+                    alt="product image"
+                />
+            </div>
 
+            <div class="flex w-full flex-row justify-between">
+                <div class="text-neutral-300 font-light">
+                    Updated: 2mins ago
+                </div>
+                <div class="text-neutral-300 font-light">headache</div>
+            </div>
+            <div class="flex flex-row justify-between">
+                <div
+                    class="text-neutral-600 leading-tight tracking-tight font-bold"
+                >
+                    Panadol
+                </div>
+                <div
+                    class="text-neutral-600 leading-tight tracking-tight font-bold"
+                >
+                    UGX: 2000
+                </div>
+            </div>
+            <div class="text-neutral-600 font-light">Lords Mercy Pharmacy</div>
+            <div class="text-neutral-600 font-light">
+                This is onr og ths jojs joijf ojaifij oaidjf ijdf ajfoiadjf
+                idfjadoifjasi iojdf dfaisojioj jdfasoidf s
+            </div>
+            <div
+                class="flex w-full justify-end text-neutral-600 leading-tight tracking-tight font-bold hover:text-blue-600"
+            >
+                <div>
+                    Contact: 0782345364
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
 import { useRoute } from "vue-router";
 export default {
-
-setup(){
+    setup() {
         const route = useRoute();
-    const routeId = route.params.id;
-    return {
-        routeId
-    }
-}
-}
+        const routeId = route.params.id;
+        return {
+            routeId,
+        };
+    },
+};
 </script>
